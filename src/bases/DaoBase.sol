7c005b1b6fff20579d26aa39b02fcf37b2ed36a4
Preston Ong <preston4896@me.com>
2024-07-05 16:22:34 +0800

    daobase hex parser

--- src/bases/DaoBase.sol
+++ src/bases/DaoBase.sol
@@ -20,4 +20,32 @@ abstract contract DaoBase {
      * itself, if the hash is included as part of the attestation data, this varies by how you define the schema.
      */
     function getCollateralHash(bytes32 attestationId) public view virtual returns (bytes32 collateralHash);
+
+    /// @dev https://github.com/Vectorized/solady/blob/4964e3e2da1bc86b0394f63a90821f51d60a260b/src/utils/JSONParserLib.sol#L339-L364
+    /// @dev Parses an unsigned integer from a string (in hexadecimal, i.e. base 16).
+    /// Reverts if `s` is not a valid uint256 hex string matching the RegEx
+    /// `^(0[xX])?[0-9a-fA-F]+$`, or if the parsed number is too big for a uint256.
+    function _parseUintFromHex(string memory s) internal pure returns (uint256 result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let n := mload(s)
+            // Skip two if starts with '0x' or '0X'.
+            let i := shl(1, and(eq(0x3078, or(shr(240, mload(add(s, 0x20))), 0x20)), gt(n, 1)))
+            for {} 1 {} {
+                i := add(i, 1)
+                let c :=
+                    byte(
+                        and(0x1f, shr(and(mload(add(s, i)), 0xff), 0x3e4088843e41bac000000000000)),
+                        0x3010a071000000b0104040208000c05090d060e0f
+                    )
+                n := mul(n, iszero(or(iszero(c), shr(252, result))))
+                result := add(shl(4, result), sub(c, 1))
+                if iszero(lt(i, n)) { break }
+            }
+            if iszero(n) {
+                mstore(0x00, 0x10182796) // `ParsingFailed()`.
+                revert(0x1c, 0x04)
+            }
+        }
+    }
 }
