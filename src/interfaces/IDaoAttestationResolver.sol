7ca447599196459526009c4270bf7f190f0ad98d
Preston Ong <preston4896@me.com>
2024-11-08 15:35:11 +0800

    resolver comment minor updates

--- src/interfaces/IDaoAttestationResolver.sol
+++ src/interfaces/IDaoAttestationResolver.sol
@@ -6,6 +6,9 @@ pragma solidity >=0.8.0;
  * @notice The resolver associates a collateral key with an attestation ID,
  * which points to the collateral data that can either be stored interanlly in the
  * Resolver contract or to an attestation registry, such as EAS.
+ * @notice The Resolver is intended to be deployed only ONCE and must be permanently immutable
+ * @notice Future upgrades/re-deployments of all other PCCS contracts can simply point to this Resolver
+ * to retain collateral data.
  */
 
 interface IDaoAttestationResolver {
